efc8f44473bd727ae0f2b8ac5d1e56a057ea95f7
BetaLixT <39497438+BetaLixT@users.noreply.github.com>
2024-10-04 10:00:28 +0400

    disabling highlight for some terraform files

--- nvim/init.lua
+++ nvim/init.lua
@@ -90,6 +90,9 @@ require'nvim-treesitter.configs'.setup {
     -- Using this option may slow down your editor, and you may see some duplicate highlights.
     -- Instead of true it can also be a list of languages
     additional_vim_regex_highlighting = false,
+		disable = function(lang, bufnr) -- Disable in large C++ buffers
+        return lang == "terraform" and vim.api.nvim_buf_line_count(bufnr) > 700
+    end,
   },
 }
 
